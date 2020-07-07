083656e28a5201aedc4facc419ee9e838ef2e5a0
Roddy Correa <ra.correa90@gmail.com>
2020-07-07 12:12:12 -0500

    add feature for investiga utpl

--- src/main/java/com/racoservices/controllers/PersonaController.java
+++ src/main/java/com/racoservices/controllers/PersonaController.java
@@ -4,26 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.racoservices.models.Medicos;
 import com.racoservices.models.Persona;
 
 @RestController
 public class PersonaController {
 	
-	@GetMapping("/personas")
+	@GetMapping("/eventos")
 	public Persona listar() {
-		return new Persona(25698, "Paul", "Ramón", 29);
+		return new Persona(25698, "INVESTIGA", "UTPL", 2020);
 	}
-
-	@GetMapping("/persona/edad/{edad}")
-    public Medicos listar2 (@PathVariable String edad){
-        return new Medicos(56, "Carlos", "Rodriguez", edad);
-	}
-
-	@GetMapping("/persona/edad/{edad}")
-    public Medicos listar3 (@PathVariable String edad){
-        return new Medicos(56, "Juanio", "Rodriguez", edad);
-	}
-	
 
 }
